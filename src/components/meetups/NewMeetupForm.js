0ba9b07bed33f1297765955d30e49bdf0025675e
wan <ichwansetyawan2015@gmail.com>
2021-03-21 17:26:18 +0700

    add some module

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,10 +1,29 @@
+import React from 'react';
+import classes from './NewMeetupForm.module.css';
 import Card from '../ui/Card';
 
+function NewMeetupForm(props) {
+    return (
+        <div>
+            <Card >
+                <form classname={classes.form}>
+                    <div classname={classes.control}>
+                        <label htmlFor="title">Meetup Title</label>
+                        <input type="text" required id="title" />
+                    </div>
+                    <div classname={classes.control}>
+                        <label htmlFor="image">Meetup Title</label>
+                        <input type="url" required id="image" />
+                    </div>
+                    <div classname={classes.control}>
+                        <label htmlFor="address">Meetup Address</label>
+                        <input type="text" required id="address" />
+                    </div>
 
-function newMeetupForm(props) {
-    return
-        <Card></Card>
-    
+                </form>
+            </Card>
+        </div>
+    );
 }
 
-export default newMeetupForm;
+export default NewMeetupForm;
